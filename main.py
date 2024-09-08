dd5dd5f219624ea5944a43f41f5cc5e8890d8669
Teun Schriks <teun.schriks@moba.net>
2024-09-08 18:45:29 +0200

    wip: retagger

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
 import sys
-from time import sleep
 
-import config
-import renamer
-import mover
-import scanner
-import extractor
-import database
+from data.settings import Settings
+from postprocessing.retagger import ReTagger
+from processing.mover import Mover
+from processing.extractor import Extractor
+from processing.renamer import Renamer
+
+# from analyzing import database, scanner
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Run command with \'main.py docker\' or \'main.py windows\'')
     else:
-        settings = config.Settings()
+        settings = Settings()
         settings.initialize(sys.argv[1])
 
-        extractor = extractor.Extractor()
-        renamer = renamer.Renamer()
-        mover = mover.Mover()
-        database = database.Database()
-        scanner = scanner.Scanner(database)
+        extractor = Extractor()
+        renamer = Renamer()
+        mover = Mover()
+        reTagger = ReTagger()
+        # database = database.Database()
+        # scanner = scanner.Scanner(database)
 
-        counter = 75
+        # counter = 75
         while True:
-            extractor.extract()
-            renamer.rename()
-            mover.move()
+            # extractor.extract()
+            # renamer.rename()
+            # mover.move()
+            reTagger.ReTag()
 
-            if counter > 72:
-                counter = 0
-                scanner.scan()
-            counter = counter + 1
-            print('sleeping for 300s')
-            sleep(300)
+            # if counter > 72:
+            #     counter = 0
+            #     scanner.scan()
+            # counter = counter + 1
+            # print('sleeping for 300s')
+            # sleep(300)
