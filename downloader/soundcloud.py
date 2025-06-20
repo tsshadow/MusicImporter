e68c43fc5ea305ca46a1c1fc26dcac0490734098
tsshadow <teunschriks@gmail.com>
2025-06-20 20:07:01 +0200

    Add `break_on_existing` parameter to downloader init
    This change allows configurable handling of existing files during downloads by introducing the `break_on_existing` parameter. It provides greater flexibility in controlling whether to skip downloads of already existing files.

--- downloader/soundcloud.py
+++ downloader/soundcloud.py
@@ -69,7 +69,7 @@ class SoundcloudDownloader:
     - Handles batch downloads with configurable parallelism and throttling.
     - Skips tracks outside a configurable duration range.
     """
-    def __init__(self, max_workers=1, burst_size=60, min_pause=1, max_pause=5):
+    def __init__(self, break_on_existing = True, max_workers=1, burst_size=60, min_pause=1, max_pause=5):
         self.output_folder = os.getenv("soundcloud_folder")
         self.archive_file = os.getenv("soundcloud_archive")
         self.cookies_file = os.getenv("soundcloud_cookies", "soundcloud.com_cookies.txt")
@@ -105,7 +105,7 @@ class SoundcloudDownloader:
             'format': 'bestaudio[ext=mp3]',
             'match_filter': self._match_filter,
             'quiet': False,
-            # 'break_on_existing': True,
+            'break_on_existing': break_on_existing,
             'set_file_timestamp': True,
             'cookies': self.cookies_file,
         }
