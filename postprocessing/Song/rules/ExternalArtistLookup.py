4c0ff97a0795d59f33ae23d3cd959ec1b4d7fc90
tsshadow <teunschriks@gmail.com>
2025-07-16 11:18:40 +0200

    Add VerifyArtistRule with heuristics

---
+++ postprocessing/Song/rules/ExternalArtistLookup.py
@@ -0,0 +1,9 @@
+class ExternalArtistLookup:
+    """Placeholder for external artist lookups via Discogs, MusicBrainz or Last.fm"""
+
+    @staticmethod
+    def is_known_artist(name: str) -> bool:
+        """Stub method to check if an artist exists on external services.
+        Returns False by default and should be implemented elsewhere.
+        """
+        return False
